af67e89c585bf23da60f5355f16c69f5b315b63d
Ben Teichman <ben.l.teichman@gmail.com>
2017-01-27 02:28:29 -0500

    test($harness): fix test harness to correctly run tape tests not mocha

---
+++ tasks.js
@@ -0,0 +1,9 @@
+import Start from 'start';
+import reporter from 'start-pretty-reporter';
+import tape from 'start-tape';
+import tapSpec from 'tap-spec';
+import { restart } from 're-start';
+
+const start = Start(reporter);
+
+module.exports = restart(start, { test: tape, testOpts: tapSpec });
