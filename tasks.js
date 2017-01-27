55555ed409b1b03e29522b0a065e79023d6afc07
Ben Teichman <ben.l.teichman@gmail.com>
2017-01-27 02:32:09 -0500

    build($reporter): instantiate reporter so it reports

--- tasks.js
+++ tasks.js
@@ -4,6 +4,6 @@ import tape from 'start-tape';
 import tapSpec from 'tap-spec';
 import { restart } from 're-start';
 
-const start = Start(reporter);
+const start = Start(reporter());
 
 module.exports = restart(start, { test: tape, testOpts: tapSpec });
