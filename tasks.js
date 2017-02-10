ed13b1f5f1748a559567d0e8fd8a23ea9c763621
Ben Teichman <ben.l.teichman@gmail.com>
2017-02-10 16:36:41 -0500

    feat(external types): support for tsconfig "types" property

--- tasks.js
+++ tasks.js
@@ -1,9 +1,5 @@
-import Start from 'start';
-import reporter from 'start-pretty-reporter';
 import tape from 'start-tape';
 import tapSpec from 'tap-spec';
 import { restart } from 're-start';
 
-const start = Start(reporter());
-
-module.exports = restart(start, { test: tape, testOpts: tapSpec });
+module.exports = restart({ test: tape, testOpts: tapSpec });
